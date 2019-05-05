742068ebbd6cf6eccc8aa5b6d8624c22cd1bd15f
dja16666 <dja16666@163.com>
2019-05-05 20:54:06 +0800

    modify

--- FrameAnalysis/FrameAnalysis/ParseSet.cs
+++ FrameAnalysis/FrameAnalysis/ParseSet.cs
@@ -5,6 +5,12 @@ namespace FrameAnalysis
 {
     public class ParseSet
     {
+        private enum ID{
+            NONE = 0,
+            AT = 1,
+            CELL = 2,
+            MOTOR = 3,
+        }
         static public string Parse(Byte[] data)
         {
             try
@@ -13,14 +19,15 @@ namespace FrameAnalysis
                 param.Param1 = "";
                 param.Param2 = "";
                 param.Param3 = "";
+                param.Param4 = "";
 
                 var id = data[4];
-                switch (id)
+                switch ((ID)id)
                 {
                     case 0:
 
                         break;
-                    case 1:
+                    case ID.AT:
                         if(data[7] == 3)
                         {
                             param.Param1 = "123";
@@ -28,9 +35,9 @@ namespace FrameAnalysis
                             param.Param3 = "123";
                         }
                         break;
-                    case 2:
+                    case ID.CELL:
                         break;
-                    case 3:
+                    case ID.MOTOR:
                         break;
                     default:
                         break;
@@ -51,5 +58,6 @@ namespace FrameAnalysis
         public string Param1 { get; set; }
         public string Param2 { get; set; }
         public string Param3 { get; set; }
+        public string Param4 { get; set; }
     }
 }
